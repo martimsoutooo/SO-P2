a506cf456531a993862711933a3e785178483d84
martimsantos <santos.martim@ua.pt>
2023-12-28 14:51:45 +0000

    chef começado

--- semaphore_restaurant/src/semSharedMemChef.c
+++ semaphore_restaurant/src/semSharedMemChef.c
@@ -127,6 +127,8 @@ static void waitForOrder ()
 {
 
     //TODO insert your code here
+    sh->fSt.st.chefStat = WAIT_FOR_ORDER;
+    saveState(nFic, &sh->fSt); 
      
     if (semDown (semgid, sh->mutex) == -1) {                                                      /* enter critical region */
         perror ("error on the up operation for semaphore access (PT)");
@@ -134,6 +136,9 @@ static void waitForOrder ()
     }
 
     //TODO insert your code here
+    sh->fSt.foodOrder = 0;
+    sh->fSt.st.chefStat = COOK;
+    saveState(nFic, &sh->fSt);
     
 
     if (semUp (semgid, sh->mutex) == -1) {                                                      /* exit critical region */
